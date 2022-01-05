b035370f80e3bad5d225327115e389f07f36e360
Anton Syrayezhkin <patrick2490as@gmail.com>
2022-01-05 12:36:46 +0300

    viewsets and routers are added

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
+from .views import PostViewSet, UserViewSet
+# from .views import PostList, PostDetail, UserList, UserDetail
 
-from .views import PostList, PostDetail
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
 
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('', PostList.as_view()),
+#     path('<int:pk>', PostDetail.as_view()),
+# ]
